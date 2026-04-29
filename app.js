971d45a3939706767f5663bb512704fac8289de3
ishban2006 <ishaanbansal222@gmail.com>
2026-04-29 13:52:32 +0530

    Created boilerplate for the website

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const mongoURL = "mongodb://127.0.0.1:27017/wander";
 const Listing = require("./models/listings");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
  
 main()
     .then(() => {
@@ -22,6 +23,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended : true}));     //To parse data of an id
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
     res.send("Hi Sexy!!!");
